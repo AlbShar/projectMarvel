d8d4d26fea7e02d82c9fc348662c022b127c695e
AlbShar <sharipov.albert1991@gmail.com>
2023-03-27 22:45:10 +0500

    correct CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,130 +1,116 @@
-import './charInfo.scss';
-import thor from '../../resources/img/thor.jpeg';
-import { Component } from 'react';
-import MarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/errorMessage';
-import Skeleton from '../skeleton/Skeleton';
-
+import "./charInfo.scss";
+import thor from "../../resources/img/thor.jpeg";
+import { Component } from "react";
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/errorMessage";
+import Skeleton from "../skeleton/Skeleton";
 
 class CharInfo extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    state = {
-        char: {},
-        loading: false,
-        error: false
-    };
-
-    
-      marvelService = new MarvelService();
-
-      onChatLoaded = (char) => {
-        this.setState({char, loading: false});
-      }
-
-      onError = () => {
-        this.setState({loading: false, error: true});
-      };
-
-      updateCharg = () => {
-        if (!this.props.id) {
-            return;
-        }
-        this.setState({loading: true});
-        this.marvelService.getCharacter(this.props.id).then(this.onChatLoaded).catch(this.onError);
-      };
-
-      componentDidUpdate(prevProps) {
-        if (prevProps.id !== this.props.id) {
-          this.updateCharg();
-        }
-      }
-      
-
-      render() {
-        const {char, loading, error} = this.state;
-
-        const skeleton = char || loading || error ? null : <Skeleton/>;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error || !char) ? <View char={char}/> : null;
-
-        return (
-            <div className="char__info">
-                <Skeleton/>
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
-    }
-    
-}
-
-
-const View = ({char}) => {
-    const { name, description, thumbnail, homepage, wiki } = char;
-  
-    return (
-        <>
-        <div className="char__basics">
-                    <img src={thumbnail} alt="abyss"/>
-                    <div>
-                        <div className="char__info-name">{name}</div>
-                        <div className="char__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="char__descr">
-                    {description}
-                </div>
-                <div className="char__comics">Comics:</div>
-                <ul className="char__comics-list">
-                    <li className="char__comics-item">
-                        All-Winners Squad: Band of Heroes (2011) #3
-                    </li>
-                    <li className="char__comics-item">
-                        Alpha Flight (1983) #50
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #503
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #504
-                    </li>
-                    <li className="char__comics-item">
-                        AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Vengeance (2011) #4
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1963) #1
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1996) #1
-                    </li>
-                </ul>
-
-        </>
-    )
+  constructor(props) {
+    super(props);
   }
 
+  state = {
+    char: null,
+    loading: false,
+    error: false,
+  };
+
+  marvelService = new MarvelService();
+
+  onChatLoaded = (char) => {
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
+  };
+
+  updateCharg = () => {
+    if (!this.props.id) {
+      return;
+    }
+    this.setState({ loading: true });
+    this.marvelService
+      .getCharacter(this.props.id)
+      .then(this.onChatLoaded)
+      .catch(this.onError);
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.id !== this.props.id) {
+      this.updateCharg();
+    }
+  }
+
+  render() {
+    const { char, loading, error } = this.state;
+
+    const skeleton = char || loading || error ? null : <Skeleton />;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+    return (
+      <div className="char__info">
+        {skeleton}
+        {errorMessage}
+        {spinner}
+        {content}
+      </div>
+    );
+  }
+}
+
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki, comics } = char;
+  const styleEmptyImg = thumbnail.includes('image_not_available') ? {objectFit: 'contain'} : null;
+
+
+  return (
+    <>
+      <div className="char__basics">
+        <img src={thumbnail} alt="abyss" style={styleEmptyImg}/>
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">{description}</div>
+      <ComicsInfo comics={comics} />
+    </>
+  );
+};
+
+const ComicsInfo = ({ comics }) => {
+
+    if (comics.length === 0) {
+        return <div className="char__comics">There is no data about comics</div>
+    };
+
+  const liElements = comics
+    .filter((item, index) => index <= 8)
+    .map((element, index) => {
+      return (
+        <li className="char__comics-item" key={index}>
+          <a href={element.resourceURI}>{element.name}</a>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">{liElements}</ul>
+    </>
+  );
+};
 export default CharInfo;
