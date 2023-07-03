e0079728f01545f96facb99a30ad5e75f1e6580b
AlbShar <sharipov.albert1991@gmail.com>
2023-07-03 23:13:41 +0500

    add react transition group for pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,54 @@
 import AppHeader from "../appHeader/AppHeader";
-import {lazy, Suspense} from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Spinner from "../spinner/Spinner";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  createBrowserRouter,
+  useLocation,
+  useOutlet,
+} from "react-router-dom";
+import { createRef } from "react";
+import Page404 from "../pages/Page404";
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
+import SingleComicPage from "../pages/singleComicPage/SingleComicPage";
+import {
+  SwitchTransition,
+  CSSTransition,
+  TransitionGroup,
+} from "react-transition-group";
 
-const Page404 = lazy(() => import("../pages/Page404"));
-const MainPage = lazy(() => import("../pages/MainPage"));
-const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/singleComicPage/SingleComicPage"));
+const routes = [
+  { path: "/", name: "MainPage", element: <MainPage />, nodeRef: createRef() },
+  {
+    path: "/comics",
+    name: "ComicsPage",
+    element: <ComicsPage />,
+    nodeRef: createRef(),
+  },
+  { path: "*", name: "Page404", element: <Page404 />, nodeRef: createRef() },
+  {
+    path: "/comics/:comicId",
+    name: "SingleComicPage",
+    element: <SingleComicPage />,
+    nodeRef: createRef(),
+  },
+];
 
-const App = () => {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: routes.map((route) => ({
+      index: route.path === "/",
+      path: route.path === "/" ? undefined : route.path,
+      element: route.element,
+    })),
+  },
+]);
+
+function App() {
+  const location = useLocation();
 
   return (
     <div>
@@ -16,20 +56,33 @@ const App = () => {
         <div className="app">
           <AppHeader />
           <main>
-            <Suspense fallback={<Spinner/>}>
-             <Routes>
-              <Route path="/comics" element={ <ComicsPage/>}/>
-              <Route path="/" element={<MainPage/>}/>
-              <Route path="*" element={<Page404/>}/>
-              <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-            </Routes> 
-            </Suspense>
-            
+            <SwitchTransition>
+              <CSSTransition
+                key={location.key}
+                timeout={300}
+                classNames="fade"
+              >
+                <Routes location={location}>
+                  <Route exact path="/">
+                    <MainPage />
+                  </Route>
+                  <Route  path="/comics">
+                    <ComicsPage />
+                  </Route>
+                  <Route  path="/comics/:comicId">
+                    <SingleComicPage />
+                  </Route>
+                  <Route path="*">
+                    <Page404 />
+                  </Route>
+                </Routes>
+              </CSSTransition>
+            </SwitchTransition>
           </main>
         </div>
       </Router>
     </div>
   );
-};
+}
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/style.scss";
+import AppHeader from "./components/appHeader/AppHeader";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  createBrowserRouter,
+  useLocation,
+  useOutlet,
+  RouterProvider,
+} from "react-router-dom";
+import { createRef } from "react";
+import Page404 from "./components/pages/Page404";
+import MainPage from "./components/pages/MainPage";
+import ComicsPage from "./components/pages/ComicsPage";
+import SingleComicPage from "./components/pages/singleComicPage/SingleComicPage";
+import {
+  SwitchTransition,
+  CSSTransition,
+  TransitionGroup,
+} from "react-transition-group";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import './style/style.scss';
+const routes = [
+  { path: "/", name: "MainPage", element: <MainPage />, nodeRef: createRef() },
+  {
+    path: "/comics",
+    name: "ComicsPage",
+    element: <ComicsPage />,
+    nodeRef: createRef(),
+  },
+  { path: "*", name: "Page404", element: <Page404 />, nodeRef: createRef() },
+  {
+    path: "/comics/:comicId",
+    name: "SingleComicPage",
+    element: <SingleComicPage />,
+    nodeRef: createRef(),
+  },
+];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: routes.map((route) => ({
+      index: route.path === "/",
+      path: route.path === "/" ? undefined : route.path,
+      element: route.element,
+    })),
+  },
+]);
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <div>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <TransitionGroup component={null}>
+            <CSSTransition key={location.key} timeout={1300} classNames="fade">
+              <Routes location={location}>
+                <Route exact path="/" element={<MainPage />} />
+
+                <Route path="/comics" element={<ComicsPage />} />
+
+                <Route path="/comics/:comicId" element={<SingleComicPage />} />
+
+                <Route path="*" element={<Page404 />} />
+              </Routes>
+            </CSSTransition>
+          </TransitionGroup>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<RouterProvider router={router} />);
