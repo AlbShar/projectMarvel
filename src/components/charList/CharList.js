88365bb5df30ce3f7f8a589d8f005c5d9bde23f3
AlbShar <sharipov.albert1991@gmail.com>
2023-07-05 18:20:41 +0500

    add animation for CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,9 +1,11 @@
 import "./charList.scss";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, createRef } from "react";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/errorMessage";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./style.css";
 
 const CharList = (props) => {
   const [characters, setCharacters] = useState([]);
@@ -13,40 +15,43 @@ const CharList = (props) => {
 
   const itemsRef = useRef([]);
 
-  const {error, loading, getAllCharacters} = MarvelService();
-  
+  const { error, loading, getAllCharacters } = MarvelService();
+
   const renderItems = (arrayItems) => {
-      const characterItem = arrayItems.map((char, index) => {
-      const styleEmptyImg =  char.thumbnail.includes("image_not_available")
+    const characterItem = arrayItems.map((char, index) => {
+      const styleEmptyImg = char.thumbnail.includes("image_not_available")
         ? { objectFit: "contain" }
         : null;
       return (
-        <li
-          className="char__item"
-          id={index}
-          ref={(el) => itemsRef.current[index] = el}
-          key={char.id}
-          onClick={() => {
-            props.onCharSelected(char.id);
-            onFocutItem(index)
-          }}
-          tabIndex={0}
-          
-        >
-          <img
-            src={`${char.thumbnail}`}
-            alt={char.name}
-            style={styleEmptyImg}
-          />
-          <div className="char__name">{char.name}</div>
-        </li>
+        <CSSTransition key={index + 1} timeout={700} classNames="item">
+          <li
+            className="char__item"
+            id={index}
+            ref={(el) => (itemsRef.current[index] = el)}
+            key={char.id}
+            onClick={() => {
+              props.onCharSelected(char.id);
+              onFocutItem(index);
+            }}
+            tabIndex={0}
+          >
+            <img
+              src={`${char.thumbnail}`}
+              alt={char.name}
+              style={styleEmptyImg}
+            />
+            <div className="char__name">{char.name}</div>
+          </li>
+        </CSSTransition>
       );
     });
 
-    return (<ul className="char__grid">{characterItem}</ul>);
-    
-  }
-
+    return (
+      <ul className="char__grid">
+        <TransitionGroup component={null}>{characterItem}</TransitionGroup>
+      </ul>
+    );
+  };
   const view = renderItems(characters);
   const errorMessage = error ? <ErrorMessage /> : null;
   const spinner = loading && !newItemsLoading ? <Spinner /> : null;
@@ -56,44 +61,44 @@ const CharList = (props) => {
     if (characters.length < 9) {
       ended = true;
     }
-    setCharacters(state => [...state, ...characters]);
+    setCharacters((state) => [...state, ...characters]);
     setNewItemsLoading(false);
-    setOffset(offset => offset + 9);
+    setOffset((offset) => offset + 9);
     setCharEnded(ended);
   };
 
-  
   const onFocutItem = (id) => {
-    itemsRef.current.forEach(item => item.classList.remove("char__item_selected"));
-    itemsRef.current[id].classList.add('char__item_selected');
+    itemsRef.current.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    itemsRef.current[id].classList.add("char__item_selected");
   };
 
   const onRequest = (offset, initial) => {
     initial ? setNewItemsLoading(false) : setNewItemsLoading(true);
-    getAllCharacters(offset).then(onCharactersLoaded)
+    getAllCharacters(offset).then(onCharactersLoaded);
   };
 
   useEffect(() => {
     onRequest(offset, true);
-  }, [])
- 
-    return (
-      <div className="char__list">
-        {errorMessage}
-        {spinner}
-        {view}
-        <button 
-            className="button button__main button__long" 
-            style={{display: charEnded ? 'none' : 'block'}} 
-            onClick={() => onRequest(offset)} 
-            disabled={newItemsLoading}>
-          <div className="inner">
-            load more
-          </div>
-        </button>
-      </div>
-    );
-}
+  }, []);
+
+  return (
+    <div className="char__list">
+      {errorMessage}
+      {spinner}
+      {view}
+      <button
+        className="button button__main button__long"
+        style={{ display: charEnded ? "none" : "block" }}
+        onClick={() => onRequest(offset)}
+        disabled={newItemsLoading}
+      >
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 CharList.propTypes = {
   onCharSelected: PropTypes.func.isRequired,
